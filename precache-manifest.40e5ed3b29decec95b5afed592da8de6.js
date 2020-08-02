2a7c951168182e00e56f72a75f2f1ee559ef99a6
scaffeinate <sudhar.ti@gmail.com>
2020-08-01 19:34:10 -0700

    Added site

---
+++ precache-manifest.40e5ed3b29decec95b5afed592da8de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286e1f5d6e6a76ff396a7f94f65bdc82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "619ca4fa6f552c6e2c8b",
+    "url": "/static/css/main.137d91f5.chunk.css"
+  },
+  {
+    "revision": "1940cf8d16cea3933479",
+    "url": "/static/js/2.c6472487.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c6472487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619ca4fa6f552c6e2c8b",
+    "url": "/static/js/main.5091505c.chunk.js"
+  },
+  {
+    "revision": "aa87f61629e7fa94837f",
+    "url": "/static/js/runtime-main.e85615b8.js"
+  }
+]);
