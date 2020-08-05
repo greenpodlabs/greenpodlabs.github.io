ec5804583d1bb9e84680cc1c1f72d860e087621e
scaffeinate <sudhar.ti@gmail.com>
2020-08-05 01:49:51 -0700

    new build

---
+++ build/precache-manifest.c03972f59197fe1b5b4c6ace905c3945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30be81c4bf38439c9da0d9407017841a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c798c2306f9247f1463",
+    "url": "/static/css/main.fd9b33db.chunk.css"
+  },
+  {
+    "revision": "4e6e95854f9a99e4ad30",
+    "url": "/static/js/2.9d4272fd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9d4272fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c798c2306f9247f1463",
+    "url": "/static/js/main.21f4a94f.chunk.js"
+  },
+  {
+    "revision": "aa87f61629e7fa94837f",
+    "url": "/static/js/runtime-main.e85615b8.js"
+  }
+]);
